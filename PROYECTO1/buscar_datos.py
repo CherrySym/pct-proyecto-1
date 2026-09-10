80bd6a4e8e6d8a5dc0f16b5ae3bcff969655296e
Matias Fadel <matiasfadel@Matiass-MacBook-Pro.local>
2026-09-10 11:22:57 -0300

    cambio de codigo a .py de procesamiento

--- PROYECTO1/buscar_datos.py
+++ PROYECTO1/buscar_datos.py
@@ -8,16 +8,6 @@ def datos_feriados(URL, archivo_salida='PROYECTO1/datos/raw/feriados_2026.txt'):
             for dia in dias:
                 feriados.write(dia + '\n')
 
-def findes_largos_candidatos(URL, archivo_salida = 'PROYECTO1/datos/raw/findelargos_2026.txt', archivo_entrada = 'PROYECTO1/datos/raw/feriados_2026.txt'):
-    if os.path.exists(archivo_salida) and os.path.getsize(archivo_salida) > 0:
-        return
-    else:
-        with open(archivo_entrada, 'r') as feriados, open(archivo_salida, 'w') as findelargo:
-            for feriado in feriados:
-                fecha = datetime.datetime.strptime(feriado.strip(), "%Y-%m-%d")
-                if fecha.weekday() == 0 or fecha.weekday() == 4:
-                    findelargo.write(feriado.strip() + '\n')
-
 def datos_destinos():
 
     return
--- PROYECTO1/procesar_datos.py
+++ PROYECTO1/procesar_datos.py
@@ -1,6 +1,33 @@
+#PROYECTO 1 - MATIAS FADEL, ERIC DAMIRES, GABRIEL MEDEROS, NICOLAS GOBBO
+
 def filtrar_datos_clima():
 
     return
 def filtrar_lugares_de_interes():
 
     return
+
+def encontrar_finde_largo(URL, archivo_salida = 'PROYECTO1/findelargos_2026.txt', archivo_entrada = 'PROYECTO1/feriados_2026.txt'):
+    if os.path.exists(archivo_salida) and os.path.getsize(archivo_salida) > 0:
+        return
+    else:
+        with open(archivo_entrada, 'r') as feriados, open(archivo_salida, 'w') as findelargo:
+            for feriado in feriados:
+                fecha = datetime.datetime.strptime(feriado.strip(), "%Y-%m-%d")
+                if fecha.weekday() == 0:
+                    # Lunes: el finde largo arranca el sabado anterior
+                    sabado = fecha - datetime.timedelta(days=2)
+                    domingo = fecha - datetime.timedelta(days=1)
+                    dias_finde = [sabado, domingo, fecha]
+                elif fecha.weekday() == 4:
+                    # Viernes: el finde largo sigue con sabado y domingo
+                    sabado = fecha + datetime.timedelta(days=1)
+                    domingo = fecha + datetime.timedelta(days=2)
+                    dias_finde = [fecha, sabado, domingo]
+                else:
+                    continue
+                linea = ', '.join(dia.strftime("%Y-%m-%d") for dia in dias_finde)
+                findelargo.write(linea + '\n')
+
+encontrar_finde_largo(APIferiados)
+
