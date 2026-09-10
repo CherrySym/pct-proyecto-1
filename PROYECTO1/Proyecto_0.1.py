f6aead46a575eb383d4d65f36e4550b072d66c17
Gabo <gabimederos07@gmail.com>
2026-09-10 10:57:56 -0300

    Se añadió la funcion consumir_datos_coords

--- PROYECTO1/Proyecto_0.1.py
+++ PROYECTO1/Proyecto_0.1.py
@@ -24,6 +24,48 @@ def consumir_datos_feriados(URL, archivo_salida='PROYECTO1/feriados_2026.txt'):
             for dia in dias:
                 feriados.write(dia + '\n')
 
+def encontrar_finde_largo(URL, archivo_salida = 'PROYECTO1/findelargos_2026.txt', archivo_entrada = 'PROYECTO1/feriados_2026.txt'):
+    if os.path.exists(archivo_salida) and os.path.getsize(archivo_salida) > 0:
+        return
+    else:
+        with open(archivo_entrada, 'r') as feriados, open(archivo_salida, 'w') as findelargo:
+            for feriado in feriados:
+                fecha = datetime.datetime.strptime(feriado.strip(), "%Y-%m-%d")
+                if fecha.weekday() == 0 or fecha.weekday() == 4:
+                    findelargo.write(feriado.strip() + '\n')
 
 
+def consumir_datos_coords(archivo_salida='PROYECTO1/coords_2026.txt', actualizado=False):
+    archivo_entrada = 'PROYECTO1/direcciones_2026.txt'
+    HEADERS = {'User-Agent': 'proyecto1-geocoder'}
+
+    if actualizado:
+        print("Ya está actualizado, no se hicieron cambios en el archivo de salida.")
+        return
+
+    
+    with open(archivo_entrada, 'r') as f:
+        direcciones = [line.strip() for line in f.readlines() if line.strip()]
+    
+
+    
+    with open(archivo_salida, 'w') as coordenadas:
+        for direccion in direcciones:
+            params = {'q': direccion, 'format': 'json', 'limit': 1}
+            r = requests.get(APIcoords, params=params, headers=HEADERS)
+            data = r.json()
+
+            if not data:
+                print(f"No se encontraron coordenadas para: {direccion}")
+                continue
+
+            lat = data[0]['lat']
+            lon = data[0]['lon']
+            
+            coordenadas.write(f"{direccion};{lat},{lon}\n")
+            time.sleep(1)
+
+    print("Coordenadas actualizadas correctamente")
+
+consumir_datos_coords(archivo_salida='PROYECTO1/coords_2026.txt', actualizado=True)
 
